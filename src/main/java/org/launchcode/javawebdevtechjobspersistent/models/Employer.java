05b3e0f29c0e2c556f8645804defa42017e7e919
Brent Letts <brent.letts@gmail.com>
2020-03-04 14:50:30 -0600

    commit 3mar2020

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,13 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank(message = "This is a required field")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(){ }
 
@@ -17,5 +25,13 @@ public class Employer extends AbstractEntity {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
